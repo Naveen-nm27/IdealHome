d7b44d25fa6e816eb7c99955e7fcfe564ba32fad
Naveen.mn <naveenm2795@gmail.com>
2025-11-08 03:28:28 +0530

    11_08

--- UserSignUp.Designer.cs
+++ UserSignUp.Designer.cs
@@ -28,12 +28,294 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "UserSignUp";
+            groupBox1 = new GroupBox();
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            textBox3 = new TextBox();
+            label4 = new Label();
+            textBox4 = new TextBox();
+            label5 = new Label();
+            label6 = new Label();
+            comboBox1 = new ComboBox();
+            textBox5 = new TextBox();
+            label7 = new Label();
+            comboBox2 = new ComboBox();
+            label8 = new Label();
+            groupBox2 = new GroupBox();
+            comboBox3 = new ComboBox();
+            label9 = new Label();
+            textBox6 = new TextBox();
+            label10 = new Label();
+            pictureBox1 = new PictureBox();
+            label11 = new Label();
+            openFileDialog1 = new OpenFileDialog();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(label11);
+            groupBox1.Controls.Add(pictureBox1);
+            groupBox1.Controls.Add(comboBox2);
+            groupBox1.Controls.Add(label8);
+            groupBox1.Controls.Add(textBox5);
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(comboBox1);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(textBox4);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Location = new Point(12, 29);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(774, 258);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Personal Info";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(54, 17);
+            label1.TabIndex = 0;
+            label1.Text = "Sign Up";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(6, 38);
+            label2.Name = "label2";
+            label2.Size = new Size(71, 17);
+            label2.TabIndex = 1;
+            label2.Text = "First Name";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(102, 35);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(267, 25);
+            textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(102, 69);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(267, 25);
+            textBox2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(6, 77);
+            label3.Name = "label3";
+            label3.Size = new Size(70, 17);
+            label3.TabIndex = 3;
+            label3.Text = "Last Name";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(102, 110);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(267, 25);
+            textBox3.TabIndex = 6;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(6, 118);
+            label4.Name = "label4";
+            label4.Size = new Size(56, 17);
+            label4.TabIndex = 5;
+            label4.Text = "Address";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(102, 156);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(267, 25);
+            textBox4.TabIndex = 8;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(6, 164);
+            label5.Name = "label5";
+            label5.Size = new Size(31, 17);
+            label5.TabIndex = 7;
+            label5.Text = "Age";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(6, 205);
+            label6.Name = "label6";
+            label6.Size = new Size(51, 17);
+            label6.TabIndex = 9;
+            label6.Text = "Gender";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(102, 205);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(267, 25);
+            comboBox1.TabIndex = 10;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(503, 35);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(253, 25);
+            textBox5.TabIndex = 12;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(393, 38);
+            label7.Name = "label7";
+            label7.Size = new Size(104, 17);
+            label7.TabIndex = 11;
+            label7.Text = "Google Location";
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(501, 74);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(255, 25);
+            comboBox2.TabIndex = 14;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(393, 82);
+            label8.Name = "label8";
+            label8.Size = new Size(64, 17);
+            label8.TabIndex = 13;
+            label8.Text = "University";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(comboBox3);
+            groupBox2.Controls.Add(label9);
+            groupBox2.Controls.Add(textBox6);
+            groupBox2.Controls.Add(label10);
+            groupBox2.Location = new Point(12, 303);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(774, 258);
+            groupBox2.TabIndex = 15;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "University Info";
+            // 
+            // comboBox3
+            // 
+            comboBox3.FormattingEnabled = true;
+            comboBox3.Location = new Point(130, 24);
+            comboBox3.Name = "comboBox3";
+            comboBox3.Size = new Size(267, 25);
+            comboBox3.TabIndex = 14;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(6, 32);
+            label9.Name = "label9";
+            label9.Size = new Size(64, 17);
+            label9.TabIndex = 13;
+            label9.Text = "University";
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(130, 67);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(267, 25);
+            textBox6.TabIndex = 12;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(6, 70);
+            label10.Name = "label10";
+            label10.Size = new Size(104, 17);
+            label10.TabIndex = 11;
+            label10.Text = "Google Location";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(629, 110);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(127, 142);
+            pictureBox1.TabIndex = 15;
+            pictureBox1.TabStop = false;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(393, 164);
+            label11.Name = "label11";
+            label11.Size = new Size(89, 17);
+            label11.TabIndex = 16;
+            label11.Text = "Upload Photo";
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // UserSignUp
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(798, 626);
+            Controls.Add(groupBox2);
+            Controls.Add(label1);
+            Controls.Add(groupBox1);
+            Name = "UserSignUp";
+            Text = "UserSignUp";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private GroupBox groupBox1;
+        private TextBox textBox5;
+        private Label label7;
+        private ComboBox comboBox1;
+        private Label label6;
+        private TextBox textBox4;
+        private Label label5;
+        private TextBox textBox3;
+        private Label label4;
+        private TextBox textBox2;
+        private Label label3;
+        private TextBox textBox1;
+        private Label label2;
+        private Label label1;
+        private ComboBox comboBox2;
+        private Label label8;
+        private GroupBox groupBox2;
+        private ComboBox comboBox3;
+        private Label label9;
+        private TextBox textBox6;
+        private Label label10;
+        private Label label11;
+        private PictureBox pictureBox1;
+        private OpenFileDialog openFileDialog1;
     }
 }
